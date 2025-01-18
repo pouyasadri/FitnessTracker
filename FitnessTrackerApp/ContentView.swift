58b17924be92c4fe3fda7f9f1ed3fe4b61956847
Seyedpouya <seyedpouya@digi-conception.fr>
2025-01-18 17:52:44 +0100

    Initial Commit

---
+++ FitnessTrackerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitnessTrackerApp
+//
+//  Created by Pouya Sadri on 18/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitnessTrackerApp/FitnessTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerAppApp.swift
+//  FitnessTrackerApp
+//
+//  Created by Pouya Sadri on 18/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
