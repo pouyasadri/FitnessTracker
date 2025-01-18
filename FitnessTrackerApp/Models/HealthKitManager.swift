8cdaa26d7cccb2cef36f6f5f6d3d14f898aecfec
Seyedpouya <seyedpouya@digi-conception.fr>
2025-01-18 22:59:54 +0100

    first commit

---
+++ FitnessTrackerApp/Models/HealthKitManager.swift
@@ -0,0 +1,8 @@
+//
+//  HealthKitManager.swift
+//  FitnessTrackerApp
+//
+//  Created by Pouya Sadri on 18/01/2025.
+//
+
+import Foundation
---
+++ FitnessTrackerApp/ViewModels/FitnessTrackerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FitnessTrackerViewModel.swift
+//  FitnessTrackerApp
+//
+//  Created by Pouya Sadri on 18/01/2025.
+//
+
+import Foundation
